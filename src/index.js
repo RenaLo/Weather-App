8b41a1bb3bd08537bcf159f7e60df3b531c3438d
RenaLo <73947868+RenaLo@users.noreply.github.com>
2020-12-11 17:53:42 +0100

    Added weather icon of API

--- src/index.js
+++ src/index.js
@@ -65,6 +65,15 @@ function displayWeather(response) {
   document.querySelector("#time").innerHTML = formattedTime(
     response.data.dt * 1000
   );
+  document
+    .querySelector("#weather-icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#weather-icon")
+    .setAttribute("alt", response.data.weather[0].description);
 }
 //FORECAST
 let days = [
